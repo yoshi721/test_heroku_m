09aacf129a35b0d86cd2f056c69606ab7a8450f9
Your Name <you@example.com>
2019-11-10 20:13:11 +0100

    new

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, make_response
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    some_text = "Hello Message!"
+    current_year = datetime.datetime.now().year
+    cities = ["Wien", "Salzburg", "Linz", "Graz"]
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
+@app.route("/about-me", methods=["GET", "POST"])
+def about():
+    if request.method == "GET":
+        user_name = request.cookies.get("user_name")
+        return render_template("about.html", name=user_name)
+    elif request.method == "POST":
+        contact_name = request.form.get("contact-name")
+        contact_email = request.form.get("contact-email")
+        contact_message = request.form.get("contact-message")
+        print(contact_name)
+        print(contact_email)
+        print(contact_message)
+
+        response = make_response(render_template("success.html"))
+        response.set_cookie("user_name", contact_name)
+        return response
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+if __name__ == '__main__':
+    app.run()
